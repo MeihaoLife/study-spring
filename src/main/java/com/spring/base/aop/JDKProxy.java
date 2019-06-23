0eb206e2149214fd01555061f14f2e35185f2134
zhanghao <zhanghao45@example.com>
2019-06-23 22:23:11 +0800

    message

---
+++ src/main/java/com/spring/base/aop/JDKProxy.java
@@ -0,0 +1,15 @@
+package com.spring.base.aop;
+
+/**
+ * @ClassName JDKProxy
+ * @Description: TODO
+ * @Author zhanghao
+ * @Date 2019/6/23 10:10 PM
+ * @Version 1.0
+ **/
+public class JDKProxy {
+
+    public static void main(String[] args) {
+        System.out.println(1231);
+    }
+}
